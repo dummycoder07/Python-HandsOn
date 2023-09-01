064eb68c392922158b617e3edc1c162dbe276ee4
Aman <sharma.aman.sa456@gmail.com>
2023-09-01 12:34:16 +0530

    Day 1 Commit

---
+++ Day - 1/02-string-manupulation.py
@@ -0,0 +1,11 @@
+# print hello world three times
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+# New Line Concept - print hello world using one print fn
+print("Hello World!\nHello World!\nHello World!")
+
+# Concatenation
+print("Hello " + "DummyCoder!")
+print("Hello" + " " + "DummyCoder")
