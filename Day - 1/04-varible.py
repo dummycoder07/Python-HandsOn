a500a7b587ac270e35df9f76eb2478fda127ee69
Aman <sharma.aman.sa456@gmail.com>
2023-09-01 13:19:42 +0530

    Day 1

---
+++ Day - 1/04-varible.py
@@ -0,0 +1,11 @@
+# Variable
+name = input("What is your name? ")
+print(name)
+
+name = "Angela"
+print(name)
+
+# len() - To get the length of String
+name = input("What is your name? ")
+length = len(name)
+print(length)
